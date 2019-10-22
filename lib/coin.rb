d6c0a46cd5622b1dc34fd5f7b93d0024029a029e
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2019-10-22 20:53:10 +0100

    :construction: Implement classes to build coin flip program

--- lib/coin.rb
+++ lib/coin.rb
@@ -1,2 +1,26 @@
 class Coin
+
+    attr_accessor :result, :heads_scored, :coin
+
+    def initialize
+       @result = []
+    end
+
+    def flip
+        puts "You are flipping a coin"
+        if rand.round == 0
+          coin = "heads"
+        else
+          coin = "tails"
+        end
+        puts "It is " + coin
+
+        @result << coin
+    end
+
+    def heads
+        heads_scored = @result.select { |s| s == "heads" }
+        result.delete_if { |s| s == "heads" }
+        return heads_scored
+    end
 end
--- lib/scorebook.rb
+++ lib/scorebook.rb
@@ -1,2 +1,15 @@
 class Scorebook
+
+    def initialize
+        @score = []
+    end
+
+    def heads_recorded(coin)
+        @score << coin.heads
+    end
+
+    def print
+        print_array = @score.reduce(:concat)
+        return print_array.count
+    end
 end
